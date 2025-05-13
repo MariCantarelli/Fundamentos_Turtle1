956082642b3930a601948074b307c02b922694af
m2ri16 <00000845849.marina@unicap.br>
2025-05-13 10:05:23 -0300

    exercicios 1 e 2

---
+++ turtle_1.py
@@ -0,0 +1,11 @@
+import turtle 
+
+tartaruga = turtle.Turtle()
+
+lado = 100
+
+for i in range(4):
+    tartaruga.forward(lado)
+    tartaruga.right(90)
+
+turtle.done()
---
+++ turtle_2.py
@@ -0,0 +1,11 @@
+import turtle 
+
+tartaruga = turtle.Turtle()
+
+lado = 100
+
+for i in range(3):
+    tartaruga.forward(lado)
+    tartaruga.left(120)
+
+turtle.done()
