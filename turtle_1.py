9d982cde72b869502b23be2060c62ef0543b0145
m2ri16 <00000845849.marina@unicap.br>
2025-05-15 20:46:04 -0300

    exercicio 1 com correções

--- turtle_1.py
+++ turtle_1.py
@@ -1,11 +1,32 @@
 import turtle 
 
+# Criando uma tartatura 
+# Utilizando a função Turtle do módulo Turtle
+
 tartaruga = turtle.Turtle()
+
+#alterando o formato
+tartaruga.shape("turtle")
+
+#cor da tartaruga
+tartaruga.color("medium aquamarine")
 
 lado = 100
 
 for i in range(4):
-    tartaruga.forward(lado)
-    tartaruga.right(90)
+    tartaruga.forward(lado) #movimento
+    #tartaruga.back(100) movimento de volta
+    tartaruga.right(90) #rotação em graus de angulo, o left seria declarado da mesma forma
 
+
+""" 
+poderia ser feito dessa forma também
+tartaruga.forward(100)
+tartaruga.left(90)
+tartaruga.forward(100)
+tartaruga.left(90)
+tartaruga.forward(100)
+tartaruga.left(90)
+tartaruga.forward(100)
+"""
 turtle.done()
