38dc02c10d486889377ea3d236561e81adbd8880
m2ri16 <00000845849.marina@unicap.br>
2025-05-15 20:47:58 -0300

    exercicio 2 com correções

--- turtle_2.py
+++ turtle_2.py
@@ -6,6 +6,16 @@ lado = 100
 
 for i in range(3):
     tartaruga.forward(lado)
-    tartaruga.left(120)
+    tartaruga.left(120) #usamos 120 pois seria oq falta para 180 graus
+    
+"""
+ou poderia ser feito dessa forma 
+tartaruga.forward(100)
+tartaruga.left(120)
+tartaruga.forward(100)
+tartaruga.left(120)
+tartaruga.forward(100)
+    
+"""
 
 turtle.done()
