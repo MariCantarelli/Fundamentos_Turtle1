0d541d34f18ff11a6d35f81bd723cc6819e493be
m2ri16 <00000845849.marina@unicap.br>
2025-05-15 21:09:29 -0300

    exemplos

---
+++ exemplos.py
@@ -0,0 +1,14 @@
+import turtle
+
+tartaruga = turtle.Turtle()
+tartaruga.shape("turtle")
+
+#tartaruga.setpos(100, 100) movimento em x e y num gráfico
+#tartaruga.setx(100) movimento apenas no eixo x
+#tartaruga.sety(100) movimento apenas no eixo y  
+
+#tartaruga.setx(100)
+#tartaruga.sety(150)
+#tartaruga.home() volta para onde iniciou
+
+tartaruga.circle(50) #faz um circulo de acordo com o raio indicado
